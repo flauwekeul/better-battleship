8cc40b1a705a2beba78e76d1049d8c9f52914bdc
Angelo Michel <angelo.michel@iodigital.com>
2024-01-05 16:14:51 +0100

    fix: add comment

--- src/components/PlayerField.tsx
+++ src/components/PlayerField.tsx
@@ -69,6 +69,7 @@ const PlayerField = ({ playerId, isCurrentPlayer }: Props) => {
         }
       }
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [proposedShip, currentInteractiveShip, player.ships]
   );
 
